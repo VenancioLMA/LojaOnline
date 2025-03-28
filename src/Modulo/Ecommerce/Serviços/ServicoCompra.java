d61410ad1ef17024813a7cf134ef2a80007699a6
Celvet <143295987+ThiagoCelvet@users.noreply.github.com>
2025-03-28 00:09:15 -0300

    Serviço compra adicionado

--- src/Modulo/Ecommerce/Serviços/ServicoCompra.java
+++ src/Modulo/Ecommerce/Serviços/ServicoCompra.java
@@ -1,4 +1,57 @@
 package Modulo.Ecommerce.Serviços;
 
+import Modulo.Ecommerce.Entidades.Compra;
+import Modulo.Ecommerce.Entidades.ClienteCad;
+import Modulo.Ecommerce.Entidades.Carrinho;
+import Modulo.Ecommerce.Repositorios.CompraRepositorio;
+
+import java.util.List;
+
 public class ServicoCompra {
+
+    private CompraRepositorio compraRepositorio;
+
+    public ServicoCompra(CompraRepositorio compraRepositorio) {
+        this.compraRepositorio = compraRepositorio;
+    }
+
+    public Compra criarCompra(ClienteCad cliente, Carrinho carrinho) {
+        Compra compra = new Compra(false, cliente, carrinho);
+        compraRepositorio.adicionarCompra(compra);
+        return compra;
+    }
+
+    public boolean finalizarCompra(int compraId, ServicoPagamento.MetodoPagamento metodoPagamento) {
+        Compra compra = compraRepositorio.buscarCompraPorId(compraId);
+        if (compra != null) {
+            compra.setMetodoPagamento(metodoPagamento);
+            compra.finalizarCompra();
+            compraRepositorio.atualizarCompra(compra);
+            return compra.getCompra();
+        }
+        return false;
+    }
+
+    public void cancelarCompra(int compraId) {
+        Compra compra = compraRepositorio.buscarCompraPorId(compraId);
+        if (compra != null) {
+            compra.setCompra(false);
+            compraRepositorio.atualizarCompra(compra);
+        }
+    }
+
+    public Compra buscarCompraPorId(int compraId) {
+        return compraRepositorio.buscarCompraPorId(compraId);
+    }
+
+    public List<Compra> listarCompras() {
+        return compraRepositorio.listarCompras();
+    }
+
+    public void removerCompra(int compraId) {
+        Compra compra = compraRepositorio.buscarCompraPorId(compraId);
+        if (compra != null) {
+            compraRepositorio.removerCompra(compra);
+        }
+    }
 }
